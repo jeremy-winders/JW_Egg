6d7d9f896435971ec344a358867535379338eff4
Jeremy Winders <jeremy.winders@usda.gov>
2023-02-21 15:45:31 -0500

    Co-authored-by: Adam Rivers <arivers@users.noreply.github.com>

--- Egg_Group_blanks_Mean_split.py
+++ Egg_Group_blanks_Mean_split.py
@@ -4,40 +4,36 @@ import os
 from os import listdir
 
 
-#for file in os.dirlist('Segmented_data/O2/113'):pass
 
-for file in os.listdir('Segmented_data/O2/113'):
-    pass
+
+
 #errors above move on
+#for file in os.dirlist('Segmented_data/O2/113'):pass
+for file in os.listdir('Segmented_data/H2O/113'):
+    pass
 
 
+
+# try this way:
 
 # Define the directory containing the files
-directory = "Segmented_data/O2/113"
+directory = "/Users/jeremy.winders/Documents/GitHub/JW_Egg/JW_Egg/Segmented_data/H2O/113"
 
 # List the files in the directory
 files = os.listdir(directory)
 
 # Process each file
 for file in files:
-    # Construct the full path to the file
-    path = os.path.join(directory, file)
-
-    # Process the file
-    with open(path, "r") as f:
+    path = os.path.join(directory, file)#Construct the full path to the file
+    with open(path, "r") as f:# Process the file
         data = f.read()
-        # Do something with the file data
 
+#works but error AttributeError: 'str' object has no attribute 'to_csv
+#data_Reset = data.reset_index()
+data.to_csv("Droptest_H3O_EN113_set1.csv")
 
-
-
-
-
-
-
-
-
-
+############################################################################
+###---Start over single import and export---###
 
 
 data1 = pd.read_csv("Segmented_data/H2O/113/Segmented_10cycles_Eggs_Day7_Batch_8_Calls_H3O_EN113_set1.csv")
@@ -65,20 +61,22 @@ def group_key(x):
     if "Jar_BLK_" in x:
         return "Jar_BLK"
     else:
-        return "Other"
+        return "egg"
 
 # Split the DataFrame based on the group key
-groups = data1_Drop.groupby(group_key)
+df_data1_split = data1_Drop.groupby('group_id')
+df_data1_split
+
 
 # Extract the groups
-jar_blk_group = groups.get_group("Jar_BLK")
-other_group = groups.get_group("Other")
+jar_blk_group = groups.get_group("Jar_BLK)")
+Egg_group = groups.get_group("Eggs")
 
 
 
 
-
-
+data1_1 = data1_Drop[data1_Drop['Jar_BLK_1'].notna()]
+data1_1
 
 
 
@@ -87,7 +85,48 @@ other_group = groups.get_group("Other")
 
 
 #data1_Drop.to_csv("Droptest_H3O_EN113_set1.csv")
-# for each file process
+# can't export because of the group_key error, so did it manually and then imported the file
+
+data1_onlyEgg = pd.read_csv("Segmented_data/Test_Extraction/DropJars_H3O_EN113_set1.csv")
+data1_onlyJar = pd.read_csv("Segmented_data/Test_Extraction/DropEggs_H3O_EN113_set1.csv")
+
+data1_onlyEgg
+data1_onlyJar
+
+
+
+# Get mean
+data1_onlyJar_mean = data1_onlyJar.groupby(['group_id']).mean()
+data1_onlyJar_mean
+
+#cannot get mean cause of group_id error, shows not in axis issue but I don't know how to fix it
+#print(data1_onlyJar_mean.columns.tolist())
+data1_onlyJar_mean.set_index()
+
+
+#Drop unneeded columns
+data1_onlyJar_mean.drop(columns=['group_id'])
+Mean_all_Jars = data1_onlyJar_mean.drop(columns=['group_id'])
+Mean_all_Jars.head()
+Mean_all_Jars.to_csv("Segmented_data/Test_Extraction/mean_all_Jars_Day7_Batch_8_Calls_H3O_EN113_set1.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #Export the mean of the data
